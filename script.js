7e246aa191a8cb28c4886c9eea2aacf2b240ca2c
anuragsoni5643 <sonianurag506@gmail.com>
2025-01-18 01:50:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function fibonacci(num) {
 // your code here
-	if (num == 1) {
-    return 0;
-} else if (num == 2) {
-    return 1;
+	 let a = 0, b = 1;
+    for (let i = 2; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return num === 0 ? a : b;
 }
-    return fibonacci(num - 1) + fibonacci(num - 2);
-}
-
 
 
 module.exports = fibonacci;
