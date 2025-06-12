0a9b776813aa2090545e224caf27e5169c128961
anuragsoni5643 <sonianurag506@gmail.com>
2025-06-12 10:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) return 0; // First Fibonacci number
+    if (num === 1) return 0; // Second Fibonacci number
+    if (num === 2) return 1; // Third Fibonacci number
 	 let a = 0, b = 1;
-    for (let i = 2; i <= num; i++) {
+    for (let i = 2; i <num; i++) {
         let temp = a + b;
         a = b;
         b = temp;
